5ab13a697e07216542e53f377bd7a95a96497bb2
Bryce Kerley <bkerley@brycekerley.net>
2016-11-06 11:43:34 -0500

    basic

--- entry/src/main.rs
+++ entry/src/main.rs
@@ -1,3 +1,22 @@
+extern crate iron;
+extern crate mount;
+extern crate staticfile;
+
+use std::path::Path;
+
+use iron::prelude::*;
+
+use mount::Mount;
+
+use staticfile::Static;
+
+fn mount() -> Mount {
+    let mut mount = Mount::new();
+    mount.mount("/", Static::new(Path::new("priv/www")));
+
+    mount
+}
+
 fn main() {
-    println!("Hello, world!");
+    Iron::new(mount()).http("localhost:8080").unwrap();
 }
