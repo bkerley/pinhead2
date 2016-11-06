e33c45ab16091a52f8d861f12ce1aa7a550b5cb1
Bryce Kerley <bkerley@brycekerley.net>
2016-11-06 12:27:42 -0500

    start level1

--- level1/src/main.rs
+++ level1/src/main.rs
@@ -1,3 +1,32 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("enter a guess");
+
+    let mut guess = String::new();
+
+    io::stdin().
+        read_line(&mut guess).
+        expect("failed to read guess");
+
+    let check:u64 = guess.
+        trim().
+        as_bytes().
+        iter().
+        enumerate().
+        map(|(i, b)| (*b as u64) << ((i as u64) * 2)).
+        sum();
+
+    let sum:u64 = guess.
+        as_bytes().
+        iter().
+        map(|b| (*b as u64) + 219).
+        sum();
+
+    if check == 578674789 {
+        println!("congratulations!");
+        println!("http://10.219.2.1:{}", sum);
+    } else {
+        println!("sorry, didn't match");
+    }
 }
