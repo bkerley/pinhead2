817a3cb153407b43cbb54a9504220680b54054a0
Bryce Kerley <bkerley@brycekerley.net>
2017-01-13 11:04:03 -0500

    one last signpost fix, heh

--- signpost/signpost.rb
+++ signpost/signpost.rb
@@ -1,7 +1,7 @@
 require 'socket'
 
 sock = UDPSocket.new
-sock.connect "localhost", 1337
+sock.connect "pinhead2", 1337
 sock.send "87551ccb2dd746aa54a2a27716352936", 0
 mesg, addr = sock.recv 256, 0
 
